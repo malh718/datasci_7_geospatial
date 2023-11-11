51d53b5b642398e9443233e1c0e4edc913c4dfc8
maliha <maliha.718@gmail.com>
2023-11-11 18:20:48 +0000

    SUCESSgit add .git add .

--- ge.py
+++ ge.py
@@ -1,9 +1,6 @@
 import requests
 import pandas as pd
-import numpy as np
 import re
-import geopandas as gpd
-import matplotlib.pyplot as plt
 import urllib.parse
 import os
 from dotenv import load_dotenv
@@ -11,21 +8,17 @@ from dotenv import load_dotenv
 load_dotenv()
 api_key = os.getenv("GOOGLE_MAPS_API")
 
-
-
 list_of_address = pd.read_csv("slim.csv")
-list_of_address['GEO']= list_of_address['ADDRESS']+''+ list_of_address['CITY']+''+list_of_address['STATE']
-list_of_address_sample= list_of_address.sample(100)
+list_of_address['GEO'] = list_of_address['ADDRESS'] + ' ' + list_of_address['CITY'] + ' ' + list_of_address['STATE']
+list_of_address_sample = list_of_address.sample(100)
 
-google_response=[]
+google_response = []
 
-
-for address in list_of_address_sample['GEO']: 
+for address in list_of_address_sample['GEO']:
     search = 'https://maps.googleapis.com/maps/api/geocode/json?address='
     location_raw = address
     location_clean = urllib.parse.quote(location_raw)
     url_request_part1 = search + location_clean + '&key=' + api_key
-
 
     response = requests.get(url_request_part1)
     response_dictionary = response.json()
@@ -37,8 +30,8 @@ for address in list_of_address_sample['GEO']:
         final = {'address': address, 'lat': lat_response, 'lon': lng_response}
         google_response.append(final)
         print(f'Finished with {address}')
+    else:
+        print(f'No results for {address}')
 
-
-dfnew= pd.DataFrame(google_response)
-
-dfnew.to_csv('geoSPATIAL2.csv')
+dfnew2 = pd.DataFrame(google_response)
+dfnew2.to_csv('ge.csv')
